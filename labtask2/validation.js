c20036935b7d42e0f29c69968ef38fc158258d26
DibajitRoy <dibajit.2003@gmail.com>
2026-03-06 23:15:12 +0600

    task1

--- labtask2/validation.js
+++ labtask2/validation.js
@@ -1,18 +1,18 @@
-funtion data ()
+function data()
 {
- var a = document.getElementById("n1").Value;
- var b = document.getElementById("n2").Value;
- var c = document.getElementById("n3").Value;
- var d = document.getElementById("n4").Value;
- var e = document.getElementById("n5").Value;
-  if (a=="" || b=="" || c=="" || d=="" || e=="")
+ var a = document.getElementById("n1").value;
+ var b = document.getElementById("n2").value;
+ var c = document.getElementById("n3").value;
+ var d = document.getElementById("n4").value;
+ var e = document.getElementById("n5").value;
+
+ if(a=="  " || b=="" || c=="" || d=="" || e=="")
  {
     alert("Field Value need to be filled up");
     return false;
  }
  else
  {
-    alert("Data submitted successfully");
     return true;
  }
 }
