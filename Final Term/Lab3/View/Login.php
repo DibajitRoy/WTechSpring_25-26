32dd8d6715733ad4848664f786e085844f4c4ff6
DibajitRoy <dibajit.2003@gmail.com>
2026-04-26 23:00:27 +0600

    first

---
+++ Final Term/Lab3/View/Login.php
@@ -0,0 +1,31 @@
+<?php include "../Controller/loginvalidation.php"; ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Login</h2>
+
+<form method="post">
+
+<table>
+<tr>
+<td>Email:</td>
+<td><input type="text" name="email"></td>
+</tr>
+
+<tr>
+<td>Password:</td>
+<td><input type="password" name="password"></td>
+</tr>
+
+<tr>
+<td></td>
+<td><input type="submit" name="login" value="Login"></td>
+</tr>
+</table>
+
+</form>
+
+</body>
+</html>
